418f36d7ebc2a94ebabf05109eb17d632470600d
Jacob Moody <moody@posixcafe.org>
2024-04-07 01:27:08 +0000

    libc/libap: update power64 entrypoints to new _callmain standard

--- sys/src/ape/lib/ap/power64/main9.s
+++ sys/src/ape/lib/ap/power64/main9.s
@@ -1,21 +1,13 @@
-#define NPRIVATES	16
+GLOBL	_tos(SB), $8
 
-GLOBL	_plan9err(SB), $4
-
-TEXT	_main(SB), 1, $(2*8 + NPRIVATES*8)
-
+TEXT	_main(SB), 1, $-8
+	ADD	$-16, R1
+	MOVD	$0, R0
 	MOVD	$setSB(SB), R2
 	MOVD	R3, _tos(SB)
 
-	MOVD	$p-64(SP), R4
-	MOVD	R4, _privates+0(SB)
-	MOVW	$16, R4
-	MOVW	R4, _nprivates+0(SB)
-
-	MOVW	inargc-8(FP), R3
-	MOVD	$inargv+0(FP), R4
-	MOVW	R4, 16(R1)
-	BL	main(SB)
-loop:
-	BL	exit(SB)
-	BR	loop
+	MOVD	$_apemain(SB), R3
+	MOVD	R0, LR
+	MOVD	$_callmain(SB), R4
+	MOVD	R4, CTR
+	BR	(CTR)
