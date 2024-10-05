394e9c223572b14d766c25fd864c621d7015d1a8
Ori Bernstein <ori@eigenstate.org>
2024-10-05 00:06:50 +0000

    libc/runeistype: accept out of range runes
    out of range runes should just return 0 for whether
    they're within any of the classes.

--- sys/src/libc/port/runeistype.c
+++ sys/src/libc/port/runeistype.c
@@ -6,35 +6,47 @@
 int
 isspacerune(Rune c)
 {
+	if(c < 0 || c > Runemax)
+		return 0;
 	return (mergedlkup(c) & Lspace) == Lspace;
 }
 
 int
 isalpharune(Rune c)
 {
+	if(c < 0 || c > Runemax)
+		return 0;
 	return (mergedlkup(c) & Lalpha) == Lalpha;
 }
 
 int
 isdigitrune(Rune c)
 {
+	if(c < 0 || c > Runemax)
+		return 0;
 	return (mergedlkup(c) & Ldigit) == Ldigit;
 }
 
 int
 isupperrune(Rune c)
 {
+	if(c < 0 || c > Runemax)
+		return 0;
 	return (mergedlkup(c) & Lupper) == Lupper;
 }
 
 int
 islowerrune(Rune c)
 {
+	if(c < 0 || c > Runemax)
+		return 0;
 	return (mergedlkup(c) & Llower) == Llower;
 }
 
 int
 istitlerune(Rune c)
 {
+	if(c < 0 || c > Runemax)
+		return 0;
 	return (mergedlkup(c) & Ltitle) == Ltitle;
 }
