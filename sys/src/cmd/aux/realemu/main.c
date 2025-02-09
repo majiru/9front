631ed0a296fbb056a1be833377f734803de6aa14
cinap_lenrek <cinap_lenrek@felloff.net>
2025-02-09 12:53:06 +0000

    aux/realemu: don't put newline in sysfatal()

--- sys/src/cmd/aux/realemu/main.c
+++ sys/src/cmd/aux/realemu/main.c
@@ -372,7 +372,7 @@ cpuinit(void)
 		off = (ulong)i << 16;
 		seek(realmemfd, off, 0);
 		if(readn(realmemfd, memory + off, 0x10000) != 0x10000)
-			sysfatal("read real mem %lux: %r\n", off);
+			sysfatal("read real mem %lux: %r", off);
 	}
 
 	cpu.ic = 0;
